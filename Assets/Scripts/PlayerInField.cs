ef766195ac2c752b740b63db97efba6b152602b9
Esrael Santillan <esrael@secret6.com>
2015-01-24 04:47:02 +0800

    Created walking animations to player.

--- Assets/Scripts/PlayerInField.cs
+++ Assets/Scripts/PlayerInField.cs
@@ -3,8 +3,17 @@ using System.Collections;
 
 public class PlayerInField : MonoBehaviour {
 
+	enum PlayerAniMove{
+		IDLE,
+		WALK_UP,
+		WALK_DOWN,
+		WALK_RIGHT,
+		WALK_LEFT
+	};
+
 	public float m_speed = 1;
 	public float m_speedLimit = 1;
+	public Animator m_animator = null;
 
 	Transform m_camera = null;
 
@@ -14,12 +23,16 @@ public class PlayerInField : MonoBehaviour {
 		if(m_camera == null){
 			m_camera = GameObject.Find("Camera").transform;
 		}
+		if(m_animator == null){
+			m_animator = transform.FindChild("walkanimation").gameObject.GetComponent<Animator>();
+		}
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+
 		Vector3 move;
 		move.x = Input.GetAxis("Horizontal");
 		move.y = Input.GetAxis("Vertical");
@@ -31,6 +44,31 @@ public class PlayerInField : MonoBehaviour {
 		newPosCam.z = m_camera.position.z;
 
 		m_camera.position = newPosCam;
+
+		//=================================
+		// set animation based on movement
+		//---------------------------------
+		PlayerAniMove aniValue = 0; //default to idle animation
+
+		if(Mathf.Abs(move.x) > Mathf.Abs(move.y)){
+			if(move.x > 0){
+				aniValue = PlayerAniMove.WALK_RIGHT;
+			}
+			else if(move.x < 0){
+				aniValue = PlayerAniMove.WALK_LEFT;
+			}
+		}
+		else {
+			if(move.y > 0){
+				aniValue = PlayerAniMove.WALK_UP;
+			}
+			else if(move.y < 0){
+				aniValue = PlayerAniMove.WALK_DOWN;
+			}
+		}
+
+		m_animator.SetInteger("move_dir", (int)aniValue);
+
 
 //		move = move * m_speed * Time.deltaTime;
 //		if(move.magnitude >= m_speedLimit){
